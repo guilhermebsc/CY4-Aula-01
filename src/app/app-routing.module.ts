ac8d6ab057d37b8d0f25e1baf4bf7a46dfda85d8
guilhermebsc <guilhermebsc2@gmail.com>
2023-08-11 22:38:18 -0300

    Add angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Pagina2Component } from './pagina2/pagina2.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'pagina2', component:Pagina2Component}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Pagina2Component } from './pagina2/pagina2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Pagina2Component
   ],
   imports: [
     BrowserModule,
